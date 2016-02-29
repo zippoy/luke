d8fe8548da7fc4c1c09ea76730a201e7de5e834b
DmitryKey <dmitry.kan@gmail.com>
2016-02-29 23:10:46 +0200

    #50 trivial fix to the NoScoringScorer.

--- src/main/java/org/getopt/luke/NoScoringScorer.java
+++ src/main/java/org/getopt/luke/NoScoringScorer.java
@@ -1,5 +1,6 @@
 package org.getopt.luke;
 
+import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.Scorer;
 
 import java.io.IOException;
@@ -17,33 +18,18 @@ public class NoScoringScorer extends Scorer {
   }
 
   @Override
-  public int advance(int doc) throws IOException {
-    return 0;
-  }
-
-  @Override
   public int docID() {
     return 0;
   }
 
   @Override
-  public int nextDoc() throws IOException {
-    return 0;
+  public int freq() throws IOException {
+      return 1;
   }
 
-    /**
-     * Returns term frequency in the current document.  Do
-     * not call this before {@link #nextDoc} is first called,
-     * nor after {@link #nextDoc} returns NO_MORE_DOCS.
-     */
-    @Override
-    public int freq() throws IOException {
-        return 1;
-    }
-
-    @Override
-    public long cost() {
-        return 0;
-    }
+  @Override
+  public DocIdSetIterator iterator() {
+    throw new UnsupportedOperationException();
+  }
 
 }
