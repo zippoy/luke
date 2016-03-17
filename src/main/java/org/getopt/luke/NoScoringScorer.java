7d7ef3eca4f5ba56b547d1c3d8c0b2ac13bd80c3
dmitry.kan <dmitry.kan@alpha-sense.com>
2016-03-17 09:17:17 +0200

    upgrade to 5.4.1

--- src/main/java/org/getopt/luke/NoScoringScorer.java
+++ src/main/java/org/getopt/luke/NoScoringScorer.java
@@ -17,11 +17,6 @@ public class NoScoringScorer extends Scorer {
   }
 
   @Override
-  public int advance(int doc) throws IOException {
-    return 0;
-  }
-
-  @Override
   public int docID() {
     return 0;
   }
@@ -31,19 +26,19 @@ public class NoScoringScorer extends Scorer {
     return 0;
   }
 
-    /**
-     * Returns term frequency in the current document.  Do
-     * not call this before {@link #nextDoc} is first called,
-     * nor after {@link #nextDoc} returns NO_MORE_DOCS.
-     */
-    @Override
-    public int freq() throws IOException {
-        return 1;
-    }
+  @Override
+  public int advance(int i) throws IOException {
+    return 0;
+  }
 
-    @Override
-    public long cost() {
-        return 0;
-    }
+  @Override
+  public long cost() {
+    return 0;
+  }
+
+  @Override
+  public int freq() throws IOException {
+    return 1;
+  }
 
 }
