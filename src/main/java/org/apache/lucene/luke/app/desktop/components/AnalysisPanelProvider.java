acb78b246879aceb3831ea94bd1be7b706853389
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-09-17 23:37:07 +0900

    [WIP] Implement analysis tab.

--- src/main/java/org/apache/lucene/luke/app/desktop/components/AnalysisPanelProvider.java
+++ src/main/java/org/apache/lucene/luke/app/desktop/components/AnalysisPanelProvider.java
@@ -3,11 +3,14 @@ package org.apache.lucene.luke.app.desktop.components;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.name.Named;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.luke.app.desktop.components.fragments.analysis.PresetAnalyzerPaneProvider;
 import org.apache.lucene.luke.app.desktop.util.ImageUtils;
 import org.apache.lucene.luke.app.desktop.util.MessageUtils;
+import org.apache.lucene.luke.models.analysis.Analysis;
+import org.apache.lucene.luke.models.analysis.AnalysisFactory;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -20,19 +23,67 @@ import javax.swing.JTextArea;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
 
 
 public class AnalysisPanelProvider implements Provider<JPanel> {
+
+  private static final String TYPE_PRESET = "preset";
+
+  private static final String TYPE_CUSTOM = "custom";
+
+  private final JPanel mainPanel = new JPanel();
 
   private final JPanel preset;
 
   private final JPanel custom;
 
+  private final JRadioButton presetRB = new JRadioButton();
+
+  private final JRadioButton customRB  = new JRadioButton();
+
+  private final JLabel analyzerNameLbl = new JLabel();
+
+  private final JTextArea inputArea = new JTextArea();
+
+  private final ListenerFunctions listeners = new ListenerFunctions();
+
+  private Analysis analysisModel;
+
+  class ListenerFunctions {
+
+    void toggleMainPanel(ActionEvent e) {
+      if (e.getActionCommand().equalsIgnoreCase(TYPE_PRESET)) {
+        mainPanel.remove(custom);
+        mainPanel.add(preset, BorderLayout.CENTER);
+      } else if (e.getActionCommand().equalsIgnoreCase(TYPE_CUSTOM)) {
+        mainPanel.remove(preset);
+        mainPanel.add(custom, BorderLayout.CENTER);
+      }
+      mainPanel.setVisible(false);
+      mainPanel.setVisible(true);
+    }
+
+  }
+
   @Inject
-  public AnalysisPanelProvider(@Named("analysis_preset") JPanel preset, @Named("analysis_custom") JPanel custom) {
+  public AnalysisPanelProvider(AnalysisFactory analysisFactory,
+                               ComponentOperatorRegistry operatorRegistry,
+                               @Named("analysis_preset") JPanel preset,
+                               @Named("analysis_custom") JPanel custom) {
     this.preset = preset;
     this.custom = custom;
+
+    this.analysisModel = analysisFactory.newInstance();
+    analysisModel.createAnalyzerFromClassName(StandardAnalyzer.class.getName());
+
+    operatorRegistry.get(PresetAnalyzerPaneProvider.PresetAnalyzerPaneOperator.class).ifPresent(operator -> {
+      operator.setPresetAnalyzers(analysisModel.getPresetAnalyzerTypes());
+      operator.setSelectedAnalyzer(analysisModel.currentAnalyzer().getClass());
+    });
   }
 
   @Override
@@ -41,32 +92,33 @@ public class AnalysisPanelProvider implements Provider<JPanel> {
     panel.setBorder(BorderFactory.createLineBorder(Color.gray));
 
     JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, createUpperPanel(), createLowerPanel());
-    splitPane.setDividerLocation(330);
+    splitPane.setDividerLocation(320);
     panel.add(splitPane);
 
     return panel;
   }
 
   private JPanel createUpperPanel() {
-    JPanel panel = new JPanel(new BorderLayout());
-    panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+    mainPanel.setLayout(new BorderLayout());
+    mainPanel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
 
-    panel.add(switcher(), BorderLayout.PAGE_START);
-    panel.add(preset, BorderLayout.CENTER);
-    //panel.add(custom, BorderLayout.CENTER);
+    mainPanel.add(switcher(), BorderLayout.PAGE_START);
+    mainPanel.add(preset, BorderLayout.CENTER);
 
-    return panel;
+    return mainPanel;
   }
 
   private JPanel switcher() {
     JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEADING));
 
-    JRadioButton presetRB = new JRadioButton(MessageUtils.getLocalizedMessage("analysis.radio.preset"));
-    presetRB.setActionCommand("preset");
+    presetRB.setText(MessageUtils.getLocalizedMessage("analysis.radio.preset"));
+    presetRB.setActionCommand(TYPE_PRESET);
+    presetRB.addActionListener(listeners::toggleMainPanel);
     presetRB.setSelected(true);
 
-    JRadioButton customRB = new JRadioButton(MessageUtils.getLocalizedMessage("analysis.radio.custom"));
-    customRB.setActionCommand("custom");
+    customRB.setText(MessageUtils.getLocalizedMessage("analysis.radio.custom"));
+    customRB.setActionCommand(TYPE_CUSTOM);
+    customRB.addActionListener(listeners::toggleMainPanel);
     customRB.setSelected(false);
 
     ButtonGroup group = new ButtonGroup();
@@ -80,33 +132,50 @@ public class AnalysisPanelProvider implements Provider<JPanel> {
   }
 
   private JPanel createLowerPanel() {
-    JPanel panel = new JPanel();
-    panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-    panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3,3 ));
+    JPanel inner1 = new JPanel(new BorderLayout());
 
     JPanel analyzerName = new JPanel(new FlowLayout(FlowLayout.LEADING));
     analyzerName.add(new JLabel(MessageUtils.getLocalizedMessage("analysis.label.selected_analyzer")));
-    analyzerName.add(new JLabel("StandardAnalyzer"));
-    panel.add(analyzerName);
+    analyzerNameLbl.setText(analysisModel.currentAnalyzer().getClass().getName());
+    analyzerName.add(analyzerNameLbl);
+    inner1.add(analyzerName, BorderLayout.PAGE_START);
 
-    JPanel input = new JPanel(new FlowLayout(FlowLayout.LEADING));
-    JTextArea inputArea = new JTextArea(2, 50);
-    input.add(new JScrollPane(inputArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
+    JPanel input = new JPanel(new FlowLayout(FlowLayout.LEADING, 5, 2));
+    inputArea.setRows(3);
+    inputArea.setColumns(50);
+    inputArea.setLineWrap(true);
+    inputArea.setWrapStyleWord(true);
+    inputArea.setText(MessageUtils.getLocalizedMessage("analysis.textarea.prompt"));
+    input.add(new JScrollPane(inputArea));
+
     JButton executeBtn = new JButton(MessageUtils.getLocalizedMessage("analysis.button.test"), ImageUtils.createImageIcon("/img/icon_lightbulb_alt.png", 20, 20));
+    executeBtn.setFont(new Font(executeBtn.getFont().getFontName(), Font.PLAIN, 15));
+    executeBtn.setMargin(new Insets(3, 3, 3, 3));
     input.add(executeBtn);
+
     JButton clearBtn = new JButton(MessageUtils.getLocalizedMessage("analysis.button.clear"));
+    clearBtn.setFont(new Font(clearBtn.getFont().getFontName(), Font.PLAIN, 15));
+    clearBtn.setMargin(new Insets(5, 5, 5, 5));
     input.add(clearBtn);
-    panel.add(input);
+
+    inner1.add(input, BorderLayout.CENTER);
+
+    JPanel inner2 = new JPanel(new BorderLayout());
 
     JPanel hint = new JPanel(new FlowLayout(FlowLayout.LEADING));
     hint.add(new JLabel(MessageUtils.getLocalizedMessage("analysis.hint.show_attributes")));
-    panel.add(hint);
+    inner2.add(hint, BorderLayout.PAGE_START);
 
     String[][] data = new String[][]{};
     String[] columnName = new String[]{"Term", "Attributes"};
     JTable tokenTable = new JTable(data, columnName);
     tokenTable.setFillsViewportHeight(true);
-    panel.add(new JScrollPane(tokenTable));
+    inner2.add(new JScrollPane(tokenTable), BorderLayout.CENTER);
+
+    JPanel panel = new JPanel(new BorderLayout());
+    panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3,3 ));
+    panel.add(inner1, BorderLayout.PAGE_START);
+    panel.add(inner2, BorderLayout.CENTER);
 
     return panel;
   }
--- src/main/java/org/apache/lucene/luke/app/desktop/components/fragments/analysis/PresetAnalyzerPaneProvider.java
+++ src/main/java/org/apache/lucene/luke/app/desktop/components/fragments/analysis/PresetAnalyzerPaneProvider.java
@@ -1,28 +1,67 @@
 package org.apache.lucene.luke.app.desktop.components.fragments.analysis;
 
+import com.google.inject.Inject;
 import com.google.inject.Provider;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.luke.app.desktop.components.ComponentOperatorRegistry;
 import org.apache.lucene.luke.app.desktop.util.MessageUtils;
 
 import javax.swing.BorderFactory;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.Collection;
 
 public class PresetAnalyzerPaneProvider implements Provider<JPanel> {
 
+  private final JComboBox<String> analyzersCB = new JComboBox<>();
+
+  class PresetAnalyzerPaneOperatorImpl implements PresetAnalyzerPaneOperator {
+
+    @Override
+    public void setPresetAnalyzers(Collection<Class<? extends Analyzer>> presetAnalyzers) {
+      String[] analyzerNames = presetAnalyzers.stream().map(Class::getName).toArray(String[]::new);
+      ComboBoxModel<String> model = new DefaultComboBoxModel<>(analyzerNames);
+      analyzersCB.setModel(model);
+    }
+
+    @Override
+    public void setSelectedAnalyzer(Class<? extends Analyzer> analyzer) {
+      analyzersCB.setSelectedItem(analyzer.getName());
+    }
+  }
+
+  @Inject
+  public PresetAnalyzerPaneProvider(ComponentOperatorRegistry operatorRegistry) {
+    operatorRegistry.register(PresetAnalyzerPaneOperator.class, new PresetAnalyzerPaneOperatorImpl());
+  }
+
   @Override
   public JPanel get() {
-    JPanel panel = new JPanel(new GridLayout(2, 1));
-    panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 250, 3));
+    JPanel panel = new JPanel(new BorderLayout());
+    panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
 
     JLabel header = new JLabel(MessageUtils.getLocalizedMessage("analysis_preset.label.preset"));
-    panel.add(header);
+    panel.add(header, BorderLayout.PAGE_START);
 
-    JComboBox<String> analyzersCB = new JComboBox<>();
-    panel.add(analyzersCB);
+    JPanel center = new JPanel(new FlowLayout(FlowLayout.LEADING));
+    center.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    center.setPreferredSize(new Dimension(400, 40));
+    center.add(analyzersCB);
+    panel.add(center, BorderLayout.CENTER);
 
     return panel;
+  }
+
+  public interface PresetAnalyzerPaneOperator extends ComponentOperatorRegistry.ComponentOperator {
+    void setPresetAnalyzers(Collection<Class<? extends Analyzer>> presetAnalyzers);
+    void setSelectedAnalyzer(Class<? extends Analyzer> analyzer);
   }
 
 }
