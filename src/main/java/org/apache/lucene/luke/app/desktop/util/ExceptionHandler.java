349a53ff9b791248ca063f3bff6ae6d41cf22980
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-09-24 15:03:05 +0900

    fix error handler.

--- src/main/java/org/apache/lucene/luke/app/desktop/util/ExceptionHandler.java
+++ src/main/java/org/apache/lucene/luke/app/desktop/util/ExceptionHandler.java
@@ -22,53 +22,15 @@ import org.apache.lucene.luke.models.LukeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.function.Consumer;
-
 public class ExceptionHandler {
 
   private static final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
-
-  public static class ExceptionWrapper extends RuntimeException {
-    ExceptionWrapper(Throwable cause) {
-      super(cause);
-    }
-  }
-
-  @FunctionalInterface
-  public interface ConsumerWithException<T, E extends Exception> {
-    void accept(T t) throws E;
-  }
-
-  @FunctionalInterface
-  public interface RunnableWithException<E extends Exception> {
-    void run() throws E;
-  }
-
-  public static <T, E extends Exception> Consumer<T> consumerWrapper(ConsumerWithException<T, E> consumer) {
-    return t -> {
-      try {
-        consumer.accept(t);
-      } catch (Exception e) {
-        throw wrap(e);
-      }
-    };
-  }
-
-  public static <E extends Exception> Runnable runnableWrapper(RunnableWithException<E> runnable) {
-    {
-      try {
-        runnable.run();
-      } catch (Exception e) {
-        throw wrap(e);
-      }
-      return null;
-    }
-  }
 
   public static void handle(Throwable t, MessageBroker messageBroker) {
     if (t instanceof LukeException) {
       Throwable cause = t.getCause();
       String message = (cause == null) ? t.getMessage() : t.getMessage() + " " + cause.getMessage();
+      logger.warn(t.getMessage(), t);
       messageBroker.showStatusMessage(message);
     } else {
       logger.error(t.getMessage(), t);
@@ -76,11 +38,4 @@ public class ExceptionHandler {
     }
   }
 
-  private static ExceptionWrapper wrap(Exception cause) {
-    return new ExceptionWrapper(cause);
-  }
-
-  private static Throwable unwrap(ExceptionWrapper wrapper) {
-    return wrapper.getCause();
-  }
 }
