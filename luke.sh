f0cc5c3a55b60e6a33044fe348427f2bae28aea7
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-10-02 22:59:32 +0900

    Fix start script for jfx11, assembly file

--- luke.sh
+++ luke.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+if [[ -n "$PATH_TO_FX11" ]]; then
+  JAVA_OPTIONS="$JAVA_OPTIONS --module-path $PATH_TO_FX11 --add-modules=javafx.controls,javafx.fxml"
+fi
+
 if [[ -d `echo $LUKE_PATH` ]]; then
-  java -jar $LUKE_PATH/target/luke-javafx-with-deps.jar
+  nohup java $JAVA_OPTIONS -jar $LUKE_PATH/target/luke-javafx-with-deps.jar > /dev/null 2>&1 &
 else
   echo "Unable to find the LUKE_PATH environnement variable..."
   echo "Assuming you're running from the root folder of luke..."
-  nohup java -jar target/luke-javafx-with-deps.jar > /dev/null 2>&1 &
+  nohup java $JAVA_OPTIONS -jar target/luke-javafx-with-deps.jar > /dev/null 2>&1 &
 fi
-#
-# In order to start luke with custom components (a custom analyzer class extending org.apache.lucene.analysis.Analyzer or a custom Codec) run:
-# java -cp target/luke-with-deps.jar:/path/to/jar-with-custom-component.jar org.getopt.luke.Luke
-# your analyzer should appear in the drop-down menu with analyzers on the Search tab and your Codec should be automatically registered
-# java -cp target/luke-with-deps.jar:/home/dmitry/projects/github/suggestinganalyzer/target/suggestinganalyzer-1.0-SNAPSHOT.jar org.getopt.luke.Luke
+
