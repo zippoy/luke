bfd6e92923e5794678589c870bd10989efe5f2ad
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-10-03 22:07:40 +0900

    Fix the start script for windows.

--- luke.bat
+++ luke.bat
@@ -1,2 +1,5 @@
-if not "%JFX_HOME%" == "" JAVA_OPTIONS=--module-path %JFX_HOME\lib --add-modules=javafx.controls,javafx.fxml
+@echo off
+if not "%JFX_HOME%" == "" (
+    set JAVA_OPTIONS=--module-path %JFX_HOME%\lib --add-modules=javafx.controls,javafx.fxml
+)
 start javaw %JAVA_OPTIONS% -jar .\target\luke-javafx-with-deps.jar
