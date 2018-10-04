fc6764cbc98709ebb2e24447a7bdc5e8d6068d69
Tomoko Uchida <tomoko.uchida@uzabase.com>
2018-10-04 22:40:52 +0900

    Do not ignore error output

--- luke.sh
+++ luke.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ -d `echo $LUKE_PATH` ]]; then
-  nohup java -jar $LUKE_PATH/target/luke-swing-with-deps.jar > /dev/null 2>&1 &
+  nohup java -jar $LUKE_PATH/target/luke-swing-with-deps.jar > /dev/null &
 else
   echo "Unable to find the LUKE_PATH environnement variable..."
   echo "Assuming you're running from the root folder of luke..."
-  nohup java -jar target/luke-swing-with-deps.jar > /dev/null 2>&1 &
+  nohup java -jar target/luke-swing-with-deps.jar > /dev/null &
 fi
