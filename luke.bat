c66f7fb066b9633e9d4847221a872be94534f3ce
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-10-12 23:43:23 +0900

    Add java options

--- luke.bat
+++ luke.bat
@@ -1,1 +1,3 @@
-start javaw -Dswing.systemlaf=com.jgoodies.looks.plastic.PlasticXPLookAndFeel -jar .\target\luke-swing-with-deps.jar
+@echo off
+set JAVA_OPTIONS=-Xmx1024m -Xms512m -XX:MaxMetaspaceSize=256m
+start javaw %JAVA_OPTIONS% -Dswing.systemlaf=com.jgoodies.looks.plastic.PlasticXPLookAndFeel -jar .\target\luke-swing-with-deps.jar
