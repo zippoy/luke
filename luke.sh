97178624799d3f65f17474578a0f3f98660d559d
Tomoko Uchida <tomoko.uchida.1111@gmail.com>
2018-10-12 23:41:27 +0900

    Add some java options, remove code duplicates, log stdout/stderr

--- luke.sh
+++ luke.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [[ -d `echo $LUKE_PATH` ]]; then
-  nohup java -jar $LUKE_PATH/target/luke-swing-with-deps.jar > /dev/null &
-else
-  echo "Unable to find the LUKE_PATH environnement variable..."
-  echo "Assuming you're running from the root folder of luke..."
-  nohup java -jar target/luke-swing-with-deps.jar > /dev/null &
+JAVA_OPTIONS="-Xmx1024m -Xms512m -XX:MaxMetaspaceSize=256m"
+JAR_FILE="target/luke-swing-with-deps.jar"
+if [[ ! -d `echo $LUKE_PATH` ]]; then
+  LUKE_PATH=$(cd $(dirname $0) && pwd)
+  echo "Unable to find the LUKE_PATH environnement variable."
+  echo "Set LUKE_PATH to $LUKE_PATH"
 fi
+
+cd ${LUKE_PATH}
+nohup java ${JAVA_OPTIONS} -jar ${JAR_FILE} > ${HOME}/.luke.d/luke_out.log 2>&1 &
